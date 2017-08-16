bcdfa32d510c09dfa9eaeaebe60c9c4630f1ee4f
David Muckle <dvdmuckle@dvdmuckle.xyz>
2017-08-15 21:30:03 -0400

    Doom module

--- lib/module/doom.go
+++ lib/module/doom.go
@@ -11,50 +11,39 @@ import (
 	"strings"
 )
 
-const doomHost = "localhost:8000/api/player/actions"
+type doomStruct struct {
+	Type string `json:"type"`
+}
 
-func Helpdoom() []string {
+const doomHost = "http://localhost:8000/api/player/actions"
+
+func HelpDoom() []string {
 	s := "doom <command> - play doom!"
 	return []string{s}
 }
 
 //Sanitize input
-func doom(cfg *configure.Config, in *message.InboundMsg, actions *Actions) {
+func Doom(cfg *configure.Config, in *message.InboundMsg, actions *Actions) {
 	doomCommand := "enter a command, dipstick"
 	if len(in.MsgArgs[1:]) == 0 {
 		actions.Say(doomCommand)
 		return
 	}
-	if strings.Join(in.MsgArgs[1:], " ") == "shoot" || "forward" || "backward" || "left" || "right" {
-		actions.Say("invalid command, comands are: shoot, forward, backward, left, right")
+	//Ugly switch statement to sanitize input because RESTful Doom doesn't
+	doomCommand = strings.Join(in.MsgArgs[1:], " ")
+	switch doomCommand {
+	case "shoot":
+	case "forward":
+	case "backward":
+	case "left":
+	case "right":
+	case "use":
+	default:
+		actions.Say("invalid command, comands are: shoot, forward, backward, left, right, use")
 		return
 	}
-	doomCommand = strings.Join(in.MsgArgs[1:], " ")
-
-	post(doomCommand)
-}
-
-//Perform actual POST
-func post(doomCommand string, actions *Actions) {
-	//There has got to be a better way to do this
-	/*switch doomCommand {
-	case "shoot":
-		body := strings.NewReader(`{"type":"shoot"}`)
-	case "forward":
-		body := strings.NewReader(`{"type":"forward"}`)
-	case "backward":
-		body := strings.NewReader(`{"type":"backward"}`)
-	case "left":
-		body := strings.NewReader(`{"type":"turn-left"}`)
-	case "right":
-		body := strings.NewReader(`{"type":"turn-right"}`)
-	case "open":
-		body := strings.NewReader(`{"type":"open"}`)
-	default:
-		return //This shouldn't actually happen since we sanitize earlier, but just in case
-	}*/
-	values := map[string]string{"type": doomCommand}
-	jsonValue, _ := json.Marshal(values)
+	doomToPost := doomStruct{Type: doomCommand}
+	jsonValue, _ := json.Marshal(doomToPost)
 	resp, err := http.Post(doomHost, "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
 		// handle err
